664d540b9a0552bb74c000202dda788dfa9dfca9
Meryem ÇİFCİ <meryem.cifci@ogr.sakarya.edu.tr>
2025-02-25 17:25:18 +0300

    footer ve navbar düzenlendi

---
+++ PresentationLayer/Controllers/ServicesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationLayer.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Surdurulebilirlik()
+        {
+            return View();
+        }
+        public IActionResult CevreDostuPlastikler()
+        {
+            return View();
+        }
+        public IActionResult NeredeGeriDonusturebilirim()
+        {
+            return View();
+        }
+        public IActionResult KarbonAyakIzi()
+        {
+            return View();
+        }
+        public IActionResult SuKaynaklari()
+        {
+            return View();
+        }
+        public IActionResult BolgeselEkonomi()
+        {
+            return View();
+        }
+    }
+}
